c3e1b6067d4a76ab0b289e8748f9a9d0e88d662f
Usatov <valeriy-usatov@yandex.ru>
2022-04-12 06:00:38 +0700

    final

--- srs/js/script.js
+++ srs/js/script.js
@@ -139,7 +139,7 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href=#up]").click(function(){
+    $("a[href='#up']").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
